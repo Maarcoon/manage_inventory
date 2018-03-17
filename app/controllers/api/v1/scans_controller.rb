487d598d0d5c57cbda2e06d8e33e4e9d118cd89a
Felipe Marcon <fmarcon.mail@gmail.com>
2018-03-16 23:48:04 -0300

    Api scans sold items

--- app/controllers/api/v1/scans_controller.rb
+++ app/controllers/api/v1/scans_controller.rb
@@ -5,6 +5,7 @@ module Api
       def create
         begin
           if params[:tipo] == 'entrada'
+            # Create product units based on product selected in app and rfids readed
             if product = Product.find_by_id(params[:produto])
               params[:rfids].each do |rfid|
                 product.product_units.create(rfid: rfid)
@@ -13,8 +14,17 @@ module Api
               return render json: {status: 'error', message: "Produto com id #{params[:produto]} não encontrado."}
             end
           elsif params[:tipo] == 'saida'
-          #TODO
+            # Update product unit status to sold based on rfid read
+            if params[:rfids].is_a? Array
+              params[:rfids].each do |rfid|
+                product_unit = ProductUnit.find_by_rfid(rfid)
+                product_unit.sold! if product_unit
+              end
+            else
+              return render json: {status: 'error', message: "O parametro rfids deve ser uma lista de rfids."}
+            end
           elsif params[:tipo] == 'inventario'
+            # Receives an array of RFIDS and update inventory items in case of item found
             if params[:rfids].is_a? Array
               inventory = Inventory.open
               params[:rfids].each do |rfid|
