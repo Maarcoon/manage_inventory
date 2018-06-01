bf05895b0741a5977848211c76a8e692f51b11fe
Felipe Marcon <fmarcon.mail@gmail.com>
2018-06-01 18:13:36 -0300

    Nova forma de importação de produtos

--- app/controllers/import_products_controller.rb
+++ app/controllers/import_products_controller.rb
@@ -8,13 +8,13 @@ class ImportProductsController < ApplicationController
   # POST /import_products
   # POST /import_products.json
   def create
-
-    spreadsheet = Roo::Excel.new(params[:file].path)
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
+    spreadsheet = Roo::Excelx.new(params[:file].path)
+    header = spreadsheet.row(32)
+    (34..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
-      # row['Ref. Principal'] / row['Código'] / row['Descrição']
-      Product.create(code: row['Ref. Principal'], name: row['Descrição']) unless Product.find_by(code: row['Ref. Principal'])
+      unless ( row['Código'].blank? || Product.find_by(code: row['Ref. Principal']) )
+        Product.create(code: row['Código'], name: row['Produto']) unless Product.find_by(code: row['Ref. Principal'])
+      end
     end
 
     redirect_to products_path, notice: 'Produtos importados.'
